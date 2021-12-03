c598acfb1fd694b4b5c5ec5ab43a27e2e81aaa0d
Aio <AioDev.JS@gmail.com>
2021-12-03 15:44:02 +0900

    인피니티 스크롤 테스트

--- src/pages/offline/search.tsx
+++ src/pages/offline/search.tsx
@@ -7,14 +7,16 @@ import { useShopsInfinite } from '@/state/swr/shops/useShops'
 import { CommonFSW } from '@/types/common'
 import { PhoneIcon } from '@chakra-ui/icons'
 import { Box, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useImmer } from 'use-immer'
+
 export default function OfflineSearch() {
   const { position } = usePosition()
   const [customPosition, setCustomPosition] = useState(position)
-  const [distance, setDistance] = useState(1000)
+  const [distance, setDistance] = useState(100000)
+  const ref = useRef<HTMLDivElement>(null)
 
-  const [commonFSW, updateCommonFSW] = useImmer<CommonFSW>({ per_page: 4, sort: ['d.dis'] })
+  const [commonFSW, updateCommonFSW] = useImmer<CommonFSW>({ per_page: 10, sort: ['d.dis'] })
 
   const { data, isValidating, error, mutate, setSize, size } = useShopsInfinite(
     commonFSW,
@@ -23,14 +25,30 @@ export default function OfflineSearch() {
       : undefined,
     customPosition ? false : true
   )
-  console.log('flat전 데이터', size, data)
   const shopsData = data?.flatMap((data) => data.data)
 
   useEffect(() => {
     setCustomPosition(position)
   }, [position])
 
-  console.log('shopsData', shopsData)
+  useEffect(() => {
+    let observer: IntersectionObserver
+    if (ref.current) {
+      observer = new IntersectionObserver(
+        () => {
+          console.log('intersection!', size)
+          setSize((pre) => pre + 1)
+        },
+        {
+          threshold: 1,
+        }
+      )
+      observer.observe(ref.current)
+    }
+    return () => observer && observer.disconnect()
+  }, [ref, setSize])
+
+  // console.log('shopsData', shopsData)
 
   return (
     <MobileDefaultLayout boxProps={{ px: '8px' }}>
@@ -44,18 +62,14 @@ export default function OfflineSearch() {
         commonFSW={commonFSW}
         updateCommonFSW={updateCommonFSW}
       ></ScrollYFilterForShop>
-      <button
-        onClick={() => {
-          setSize(size + 1)
-        }}
-      >
-        test
-      </button>
-      {shopsData ? (
-        shopsData.map((shop, idx) => <ShopCard shop={shop} key={idx} />)
-      ) : (
-        <Box>No Data</Box>
-      )}
+      <Box position="relative">
+        {shopsData ? (
+          shopsData.map((shop, idx) => <ShopCard shop={shop} key={idx} />)
+        ) : (
+          <Box>No Data</Box>
+        )}
+        <Box position="absolute" bottom="600px" height="0" bgColor="black" ref={ref}></Box>
+      </Box>
     </MobileDefaultLayout>
   )
 }
